4aec6f615a8126b55a9402167abd5adf8a2b9826
AxeTrack <frank.schlosser@outlook.com>
2015-03-29 21:50:09 +0200

    Startingpoint

---
+++ public/controllers/controller.js
@@ -0,0 +1,4 @@
+var printScheduleApp = angular.module('printScheduleApp', []);
+printScheduleApp.controller('ScheduleCtrl', ['$scope', '$http', function($scope, $http) {
+	console.log("Hello World from controller");
+}]);
