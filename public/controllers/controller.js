cc0f7694e58c03bf15b85e949152cce3314eb0e4
AxeTrack <frank.schlosser@outlook.com>
2015-03-30 15:53:10 +0200

    Added CRUD-Functions

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -3,23 +3,56 @@ printScheduleApp.controller('ScheduleCtrl', ['$scope', '$http', function($scope,
 
 	console.log('Hello World from controller');
 	
-	var refesh = function () {
+	$scope.isUpdateOnTheWay = false;
+
+	var refresh = function () {
 		$http.get('/contactlist').success(function (response) {
 			console.log('I got the data I requested');
 			$scope.contactlist = response;
 			$scope.contact = "";
-
+			$scope.isUpdateOnTheWay = false;
 		});
 	};
 
-	refesh();
+	refresh();
 
-	$scope.addContact = function() {
+	$scope.add = function() {
 		console.log($scope.contact);
-		$http.post('/contactlist', $scope.contact).success(function (response) {
-			console.log(response);
-			refesh();
+		$scope.isUpdateOnTheWay = false;
+		if ($scope.contact) {		
+			$http.post('/contactlist', $scope.contact).success(function (response) {
+				console.log(response);
+				refresh();
+			});	
+		}
+	};
+
+	$scope.remove = function (id) {
+		console.log(id);	
+		$http.delete('/contactlist/' + id).success(function (response) {
+			refresh();
 		});
+	};
+
+	$scope.edit = function(id) {
+		console.log(id);
+		$scope.isUpdateOnTheWay = true;
+		$http.get('/contactlist/' + id).success(function (response) {
+			$scope.contact = response;
+		});
+	};
+
+	$scope.update = function() {
+		console.log($scope.contact._id);
+		$scope.isUpdateOnTheWay = true;
+		$http.put('/contactlist/' + $scope.contact._id, $scope.contact).success(function (response) {
+			refresh();
+		});
+	};
+
+	$scope.deselect = function() {
+		$scope.isUpdateOnTheWay = false;
+		$scope.contact = "";
 	};
 
 }]);
--- server.js
+++ server.js
@@ -14,6 +14,7 @@ var db = mongojs('contactlist', ['contactlist']);
 // set the static files location /public/img will be /img for users
 app.use(express.static(__dirname + '/public'));
 
+// parse application/json 
 app.use(bodyParser.json());
 
 // get list DATA
@@ -31,7 +32,37 @@ app.get('/contactlist', function (req, res) {
 app.post('/contactlist', function (req, res) {
 	console.log(req.body);
 	db.contactlist.insert(req.body, function (err, doc) {
+			res.json(doc);
+	});
+});
+
+// delete :id
+app.delete('/contactlist/:id', function (req, res) {
+	var id = req.params.id;
+	console.log(id);
+	db.contactlist.remove({_id: mongojs.ObjectId(id)}, function (err, doc) {
 		res.json(doc);
+	});
+});
+
+// edit :id
+app.get('/contactlist/:id', function (req, res) {
+	var id = req.params.id;
+	console.log(id);
+	db.contactlist.findOne({_id: mongojs.ObjectId(id)}, function (err, doc) {
+		res.json(doc);
+	});
+});
+
+// update :id
+app.put('/contactlist/:id', function (req, res) {
+	var id = req.params.id;
+	console.log(req.body.name);
+	db.contactlist.findAndModify({
+		query: {_id: mongojs.ObjectId(id)},
+		update: {$set: {name: req.body.name, email: req.body.email, number: req.body.number}},			
+		new: true}, function (err, doc) {
+			res.json(doc);
 	});
 });
 
