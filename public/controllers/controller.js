62ce732248d92149c5bbacbca6ee950964673cf3
AxeTrack <frank.schlosser@outlook.com>
2015-03-30 09:58:09 +0200

    Added CRUD-Functions

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,4 +1,25 @@
 var printScheduleApp = angular.module('printScheduleApp', [ 'ui.bootstrap.tpls' ]);
 printScheduleApp.controller('ScheduleCtrl', ['$scope', '$http', function($scope, $http) {
-	console.log("Hello World from controller");
+
+	console.log('Hello World from controller');
+	
+	var refesh = function () {
+		$http.get('/contactlist').success(function (response) {
+			console.log('I got the data I requested');
+			$scope.contactlist = response;
+			$scope.contact = "";
+
+		});
+	};
+
+	refesh();
+
+	$scope.addContact = function() {
+		console.log($scope.contact);
+		$http.post('/contactlist', $scope.contact).success(function (response) {
+			console.log(response);
+			refesh();
+		});
+	};
+
 }]);
--- server.js
+++ server.js
@@ -1,7 +1,43 @@
-var express = require("express");
+
+// server.js
+
+// modules =================================================
+var express = require('express');
+var mongojs = require('mongojs');
+var bodyParser = require('body-parser');
+
+// configuration ===========================================
 var app = express();
+var db = mongojs('contactlist', ['contactlist']);
 
-app.use(express.static(__dirname + "/public"));
 
+// set the static files location /public/img will be /img for users
+app.use(express.static(__dirname + '/public'));
+
+app.use(bodyParser.json());
+
+// get list DATA
+app.get('/contactlist', function (req, res) {
+	console.log('I resived a GET request');
+
+	db.contactlist.find(function (err, docs) {
+		console.log(docs);
+		res.json(docs);
+	});
+	
+});
+
+// post DATA
+app.post('/contactlist', function (req, res) {
+	console.log(req.body);
+	db.contactlist.insert(req.body, function (err, doc) {
+		res.json(doc);
+	});
+});
+
+// start app ===============================================
+// startup our app at http://192.168.59.27:3000
 app.listen(3000);
-console.log("Server running on port 3000!");
+
+// shoutout to the user
+console.log('Server running on port 3000!');
